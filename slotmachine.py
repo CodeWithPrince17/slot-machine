282745d38876f9adb89df5db4060d3a76decfce4
CodeWithPrince17 <princegodar07@gmail.com>
2026-07-03 03:23:20 +0545

    edited

--- slotmachine.py
+++ slotmachine.py
@@ -1,55 +1,58 @@
-MAX_LINES=3
-MAX_BET=100
-MIN_BET=1
-def deposit():
-    while True:
-        amount=input("what would you like to deposit? $")
-        if amount.isdigit():
-            amount=int(amount)
-            if amount > 0:
+import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
                 break
-            else:
-                print("amount must be greater than 0")
         else:
-            print("please enter a number")
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
 
-    return amount
+    return winnings, winning_lines
 
-def get_number_of_lines():
-      while True:
-        lines=input("enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
-        if lines.isdigit():
-            lines=int(lines)
-            if 1<=lines <= MAX_LINES:
-                break
-            else:
-                print("enter a valid number of lines")
-        else:
-            print("please enter a number")
 
-        return lines
-def get_bet():
-    while True:
-        amount=input("what would you like to bet? $")
-        if amount.isdigit():
-            amount=int(amount)
-            if MIN_BET<=amount<=MAX_BET:
-                break
-            else:
-                print(f"amount must be in between {MIN_BET}-{MAX_BET}.")
-        else:
-            print("please enter a number")
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
 
-    return amount
-def main():
-   balance=deposit()               
-   lines= get_number_of_lines()
-   while True:
-       bet=get_bet()
-       total_bet= bet * lines
-           
-  
-   print(f"you are betting {bet} on {lines} lines. total bet is = {total_bet}")
-   
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
 
-main() hey
+        columns.append(column)
+
+    return columns
